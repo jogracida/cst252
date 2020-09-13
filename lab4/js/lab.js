05bc72c69387ce2effb14523f0e38b6916badeb6
jogracida <65100040+jogracida@users.noreply.github.com>
2020-09-12 21:34:55 -0700

    Lab 4- Psuedocode and Problem Solving
    New files

---
+++ lab4/js/lab.js
@@ -0,0 +1,10 @@
+// Create a 8x8 board
+  // Black and red squares
+    // Every other square is red
+      // Define player pieces
+        // Take user input
+          // Authenticate user input
+            //Take AI/Player input
+              // Validate
+                // check win condition
+        // If no win, reset to step 5
