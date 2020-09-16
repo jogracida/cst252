1310a9b4c8f26bac08c5ebda398077cdcfde1001
jogracida <65100040+jogracida@users.noreply.github.com>
2020-09-16 00:59:04 -0700

    Lab 5 javascript

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+//  * Author: Jonathan Gracida <jogracida@csumb.edu>
+//  * Created: 15 September
+//  * License: Public Domain
+
+
+// Define Variables
+
+make = "Toyota";
+model = "Corolla";
+color = "Silver";
+year = 2009;
+
+// calculate
+age = 2020 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
