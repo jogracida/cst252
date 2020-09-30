1cf4511a7d10282312e7c9199d4c05b8da2406a6
jogracida <65100040+jogracida@users.noreply.github.com>
2020-09-30 12:13:31 -0700

    Lab 9 (Javascript for the Web)

---
+++ lab9/js/lab9.js
@@ -0,0 +1,25 @@
+  /**
+Author: Jonathan Gracida <jogracida@csumb.edu>
+Created: 19 September
+License: Public Domain
+**/
+
+// Make variable for the element
+var outputEl =
+document.getElementById("output");
+
+// create element "p" in "output" give it text, then Append as a child
+var new1El =
+document.createElement("p");
+
+new1El.innerHTML = "This is the actual text for the tag";
+
+outputEl.appendChild(new1El);
+
+// create new element and assign it a variable
+var new2El =
+document.createElement("p");
+
+new2El.innerHTML = "This is the new text for the tag";
+
+outputEl.appendChild(new2El);
