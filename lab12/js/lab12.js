5c6ae03dc7af038e7fc44469ffb7c03eb95c4376
jogracida <65100040+jogracida@users.noreply.github.com>
2020-10-12 13:27:46 -0700

    Updated Lab 12.Js

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -14,26 +14,26 @@
 
     //make tag for outputs
     var outputEl = document.getElementById("script-output");
-    var newEl = document.createElement("p");
-    outputEl.appendChild(newEl);
-    console.log(input);
-    var outputString = '';
-    if (input%7 == 0) {
-      newEl.innerHTML = input + "FizzBuzz";
-      outputEl.appendChild(newEl);
-      console.log("FizzBuzz");
-    } else if (input%3 == 0) {
-      newEl.innerHTML = input + "Fizz";
-      outputEl.appendChild(newEl);
-      console.log("Fizz")
-    } else if (input%5 == 0) {
-      newEl.innerHTML = input + "Buzz";
-      outputEl.appendChild(newEl);
-      console.log("Buzz")
-    } else {
-      console.log("Number: " + input);
-    }
-}
+     var newEl = document.createElement("p");
+     outputEl.appendChild(newEl);
+     console.log(input);
+     var outputString = '';
+     if (input%7 == 0) {
+       newEl.innerHTML = input + " FizzBuzz";
+       outputEl.appendChild(newEl);
+       console.log("FizzBuzz")
+     } else if (input%3 == 0) {
+       newEl.innerHTML = input + " Fizz";
+       outputEl.appendChild(newEl);
+       console.log("Fizz")
+     } else if (input%5 == 0) {
+       newEl.innerHTML = input + " Buzz";
+       outputEl.appendChild(newEl);
+       console.log("Buzz")
+     } else {
+       console.log("Number: " + input)
+     }
+   }
 // addNewParagraphToOutput(outputString);
 //     return;
 // }
